328a382f4c9e23d84d68392963905253d4686d89
Rachel Opperman <oppermanre17@gmail.com>
2025-06-11 18:30:14 -0400

    Adds error page

---
+++ app/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout name="default">
+    <div class="py-16 px-4 sm:py-24 sm:px-6 md:grid md:place-items-center lg:px-8">
+      <div class="max-w-max mx-auto">
+        <div class="flex flex-col sm:flex-row space-y-4 sm:space-x-6">
+          <p class="font-bold! text-4xl text-brand-orange! sm:text-5xl sm:leading-12!">
+            404
+          </p>
+          <div class="space-y-4 sm:pl-6 sm:border-l sm:border-brand-slate-400">
+            <h1 class="font-bold text-4xl text-brand-slate-400 sm:text-5xl">
+              Page not found.
+            </h1>
+            <p class="text-brand-slate-100!">
+              Please check the URL in the address bar and try again.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
